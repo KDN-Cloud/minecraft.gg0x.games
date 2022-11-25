37714f4dfc6ba76125897b437a703f1665c21b15
Tony <hello@kode.email>
2022-11-25 14:07:42 -0800

    init commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="description" content="GG0x Minecraft Proudly Powered by Bedrock Server">
+<meta name="keywords" content="">
+<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,user-scalable=yes"/>
+<meta property="og:title" content="GG0x Minecraft Server" />
+<meta property="og:description" content="GG0x Minecraft Proudly Powered by Bedrock Server." />
+<meta property="og:image" content="http://minecraft.gg0x.games/img/minecraft-logo.png" />
+<meta http-equiv="refresh" content="300; url=https://discord.gg/W4tWjUcmEy" />
+
+<title>GG0x Minecraft Server</title>
+<link rel="icon" href="img/minecraft-logo.png"/>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap');
+html, body {
+  height: 100%;
+  background-image: url("img/minecraft-landscape.jpg");
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-color: #000;
+  background-repeat: no-repeat;
+}
+
+img { padding: 4%; }
+p { color: #00eaff; font-family: 'Dancing Script', cursive, sans-serif; }
+
+footer {
+  font-size: 150%;
+  font-weight: bold;
+  text-align: center;
+  font-family: 'Dancing Script', cursive, sans-serif;
+  margin-left: 40%;
+  position: fixed;
+  bottom: 0;
+}
+
+.wrapper { min-height: 100%; margin: 0 auto -155px; }
+#stats { font-family: cursive, sans-serif; font-size: 80%; font-weight: 50; text-decoration: none; }
+a:link { color: orange; }
+a:visited { color: orange; }
+a:hover { color: #3bd671; }
+</style>
+
+<!-- Matomo respects your privacy -->
+<script type="text/javascript">
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDoNotTrack", true]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://stats.koderoot.net/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '19']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="https://stats.koderoot.net/matomo.php?idsite=19&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Matomo Code -->
+
+<!-- Matomo Image Tracker-->
+<noscript><p><img referrerpolicy="no-referrer-when-downgrade" src="https://stats.koderoot.net/matomo.php?idsite=19&amp;rec=1" style="border:0" alt="" /> </p></noscript>
+<!-- End Matomo -->
+
+</head><body>
+
+<a href="https://discord.gg/W4tWjUcmEy"><img src="img/join-discord.png" alt="GG0x Discord Gaming Server"></a>
+
+<footer class="footer"><p>minecraft.gg0x.games <?php echo date("Y") ?> &#9679 <a id="stats" href="https://status.kdn.cloud/status/gg0x">status</a> <a href="https://twitter.com/GG0x4" target="_blank"><img width="60" height="23" src="img/twitter.png"></a> </p></footer>
+</body></html>
